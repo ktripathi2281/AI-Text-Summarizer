af25e73ee909f6c1199247ccd78146a8419db349
Kaustubh Tripathi <75520665+ktripathi2281@users.noreply.github.com>
2024-08-25 14:39:37 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="Summary" className='w-28 object-contain' />
+        <img src={summer-high-resolution-logo} alt="Summary" className='w-28 object-contain' />
       </nav>
 
       <h1 className='head_text'>Summarize articles with <span className='orange_gradient'>AI</span></h1>
